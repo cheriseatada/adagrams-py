67c77a71f433ac0dfea9dbbc1b4cd5d675480bff
Cherise <cherise.holmes@adadevelopersacademy.org>
2022-03-23 14:20:04 -0700

    complete wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,19 @@
+import random
+
 def draw_letters():
-    pass
+    letter_pool = {'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3,
+                   'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6,
+                   'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4,
+                   'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1}
+    hand = []
+    for i in range(10):
+        letter = random.choice(list(letter_pool.keys()))
+        hand.append(letter)
+        letter_pool[letter] -= 1
+        if letter_pool[letter] == 0:
+            letter_pool.pop(letter)
+    return hand
+        
 
 def uses_available_letters(word, letter_bank):
     pass
